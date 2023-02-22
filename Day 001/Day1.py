d12f5cd4f9d75586e6499b80f39a7f2728881ed3
Abubakar Abdulkadir Abubakar <113145940+aaabubakar@users.noreply.github.com>
2023-02-22 12:24:01 +0100

    Add files via upload

---
+++ Day 001/Day1.py
@@ -0,0 +1,18 @@
+#Introduction
+print (2 + 3)
+print (3 - 1)
+print (2 * 3)
+print (3/2)
+print (3 ** 2)
+print (3 % 2)
+print (3 // 2)
+
+# Checking Data Types
+print (type(10))
+print (type(3.14))
+print (type (1 + 3j))
+print (type ('Asabeneh'))
+print (type ([1,2,3]))
+print (type ({'name':'Asanbene'}))
+print (type({9.8, 3.14, 2.7}))
+print (type ((9.8, 3.14, 2.7)))
